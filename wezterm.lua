2a10455d93b2d17e9d0b7282f6b4fe5752e33367
Metoděj Janota <metodejjanota@gmail.com>
2025-02-03 23:17:48 +0100

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
+config = wezterm.config_builder()
 
-config.color_scheme = 'Blue Matrix'
-config.font_size = 19
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
-config.enable_tab_bar = false
-config.initial_cols = 100
-config.initial_rows = 35
+config = {
+	font_size = 19,
+	enable_tab_bar = false,
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "Tokyo Night",
+	automatically_reload_config = true,
+	window_decorations = "RESIZE",
+	window_close_confirmation = "NeverPrompt",
+}
 
 return config
